6b9c8203df1cd406e1239aac131b2a12c40cf055
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-04-06 00:36:49 +0200

    Added CategoryController and test for CategoryController.

---
+++ src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package guru.springframework.spring5webfluxrest.bootstrap;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repository.CategoryRepository;
+import guru.springframework.spring5webfluxrest.repository.VendorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final VendorRepository vendorRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository, VendorRepository vendorRepository) {
+        this.categoryRepository = categoryRepository;
+        this.vendorRepository = vendorRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(categoryRepository.count().block() == 0){
+            //load data
+            System.out.println("#### LOADING DATA BOOTSTRAP ####");
+
+            categoryRepository.save(Category.builder().description("Fruits").build()).block();
+            categoryRepository.save(Category.builder().description("Nuts").build()).block();
+            categoryRepository.save(Category.builder().description("Breads").build()).block();
+            categoryRepository.save(Category.builder().description("Meats").build()).block();
+            categoryRepository.save(Category.builder().description("Eggs").build()).block();
+
+            System.out.println("Loaded Categories: " + categoryRepository.count().block());
+
+            vendorRepository.save(Vendor.builder().firstName("Fisnik").lastName("Shabani").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Fisnik2").lastName("Shabani2").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Fisnik3").lastName("Shabani3").build()).block();
+            vendorRepository.save(Vendor.builder().firstName("Fisnik4").lastName("Shabani4").build()).block();
+
+            System.out.println("Loaded Vendors: " + vendorRepository.count().block());
+        }
+
+    }
+}
